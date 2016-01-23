bac8e96d46524a8b15566aeca236dee477016952
Amita Shukla <amitashukla0906@gmail.com>
2016-01-24 00:59:00 +0530

    change to regular-link

--- js/script.js
+++ js/script.js
@@ -49,14 +49,14 @@ $(document).ready(function(e) {
 	* = Controls active menu *
 	* Hover text for the last slide
 	*************************/
-	$('#slide-3 img').each(function(index, element) {
+	$('#slide-5 img').each(function(index, element) {
 		var time = new Date().getTime();
 		var oldHref = $(this).attr('src');
 		var myImg = $('<img />').attr('src', oldHref + '?' + time );
 		
 		myImg.load(function(e) {
 			img_loaded += 1;;
-			if ( img_loaded == $('#slide-3 img').length ) {
+			if ( img_loaded == $('#slide-5 img').length ) {
 				$(function() {
 					var pause = 10;
 					$(document).scroll(function(e) {
@@ -96,7 +96,7 @@ $(function() {
 	var pause = 50; // will only process code within delay(function() { ... }) every 100ms.
 	$(window).resize(function() {
 		delay(function() {
-				var gallery_images = $('#slide-3 img');
+				var gallery_images = $('#slide-5 img');
 				
 				var images_per_row = 0;
 				if ( gallery_images.length % 2 == 0 ) {
@@ -105,18 +105,18 @@ $(function() {
 					images_per_row = gallery_images.length / 2 + 1;
 				}
 				
-				var gallery_width = $('#slide-3 img').width() * $('#slide-3 img').length;
+				var gallery_width = $('#slide-5 img').width() * $('#slide-5 img').length;
 				gallery_width /= 2;
 				if ( $('#slide-3 img').length % 2 != 0 ) {
-					gallery_width += $('#slide-3 img').width();
+					gallery_width += $('#slide-5 img').width();
 				}
 				
-				$('#slide-3 .row').css('width', gallery_width );
+				$('#slide-5 .row').css('width', gallery_width );
 				
-				var left_pos = $('#slide-3 .row').width() - $('body').width();
+				var left_pos = $('#slide-5 .row').width() - $('body').width();
 				left_pos /= -2;
 				
-				$('#slide-3 .row').css('left', left_pos);
+				$('#slide-5 .row').css('left', left_pos);
 			
 			},
 			pause
@@ -176,7 +176,7 @@ function enable_arrows( dataslide ) {
 	if ( dataslide != 6 ) {
 		$('#arrow-down').removeClass('disabled');
 	}
-	if ( dataslide == 3 ) {
+	if ( dataslide == 5 ) {
 		$('#arrow-left').removeClass('disabled');
 		$('#arrow-right').removeClass('disabled');
 	}
@@ -193,8 +193,8 @@ jQuery(document).ready(function ($) {
 	mywindow = $(window);
 	htmlbody = $('html,body');
 	
-	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
-	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+	//Create a function that will be passed a slide number and then will scroll to that slide using jquery's animate. The Jquery
+	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available through the plugin.
 	function goToByScroll(dataslide) {
 		var offset_top = ( dataslide == 1 ) ? '0px' : $('.slide[data-slide="' + dataslide + '"]').offset().top;
 		
@@ -252,12 +252,12 @@ jQuery(document).ready(function ($) {
 ******************/
 jQuery(document).ready(function ($) {
 	//Cache some variables
-	var images = $('#slide-3 a');
+	var images = $('#slide-5 a');
 	
 	images.hover(
 		function(e) {
 			var asta = $(this).find('img');
-			$('#slide-3 img').not( asta ).stop(false, false).animate(
+			$('#slide-5 img').not( asta ).stop(false, false).animate(
 				{
 					opacity: .5
 				},
@@ -271,7 +271,7 @@ jQuery(document).ready(function ($) {
 			$(this).prepend(zoom);
 		},
 		function(e) {
-			$('#slide-3 img').stop(false, false).animate(
+			$('#slide-5 img').stop(false, false).animate(
 				{
 					opacity: 1
 				},
@@ -316,9 +316,9 @@ jQuery(document).ready(function ($) {
 				}
 				break;
 			case 'arrow-right':
-				offset_left = $('#slide-3 .row').offset().left - 452;
-				if ( offset_left < $('body').width() - $('#slide-3 .row').width() ) {
-					offset_left = $('body').width() - $('#slide-3 .row').width();
+				offset_left = $('#slide-5 .row').offset().left - 452;
+				if ( offset_left < $('body').width() - $('#slide-5 .row').width() ) {
+					offset_left = $('body').width() - $('#slide-5 .row').width();
 				}
 				break;
 		}
@@ -330,8 +330,8 @@ jQuery(document).ready(function ($) {
 		}
 		
 		if ( offset_left != false ) {
-			if ( $('#slide-3 .row').width() != $('body').width() ) {
-				$('#slide-3 .row').stop(false, false).animate({
+			if ( $('#slide-5 .row').width() != $('body').width() ) {
+				$('#slide-5 .row').stop(false, false).animate({
 					left: offset_left
 				}, 1500, 'easeInOutQuart');
 			}
